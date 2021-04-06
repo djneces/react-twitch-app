3e218324f165da86bbe68db770072f6b7a1353f2
djneces <j.necesanek@hotmail.com>
2021-04-06 15:01:49 +0200

    Add Redux form

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from 'redux';
+//we rename it if we wish
+import { reducer as formReducer } from 'redux-form';
 import authReducer from './authReducer';
 
 export default combineReducers({
   auth: authReducer,
+  //redux form reducer
+  form: formReducer,
 });
